ec47714e36958f08ce1e8349eadfd6aad966149e
gerardorf <gerardorf@gmail.com>
2016-07-12 11:35:36 +0200

    fix gulp error

--- code/talking-activities/gulpfile.js
+++ code/talking-activities/gulpfile.js
@@ -14,7 +14,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass(['app.scss',
         'callToAction.css'
-    ], 'app.scss');
+    ], 'public/css/app.css');
 
     mix.scripts([
         'app.js',
